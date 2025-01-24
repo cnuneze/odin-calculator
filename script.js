7e1be8e2415b9e988b3f5889abf3671dacf76d5d
cnuneze <23690843+cnuneze@users.noreply.github.com>
2025-01-23 21:35:17 -0300

    Feat: Add calculator base functions
    Added methods for all 4 main operation and operate function
    to call methods

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 console.log("there")
+const calculator = {
+    firstOperand: 0,
+    secondOperand: 0,
+    operation: '',
+    operate: function() {
+        switch (this.operation) {
+            case '+':
+                return this.addition();
+
+            case '-':
+                return this.subtraction();
+
+            case '*':
+                return this.multiplication();
+
+            case '/':
+                return this.division();
+
+            default:
+                return 0;
+
+        }
+    },
+    addition: function() {
+        return this.firstOperand + this.secondOperand;
+    },
+    subtraction: function() {
+        return this.firstOperand - this.secondOperand;
+    },
+    multiplication: function() {
+        return this.firstOperand * this.secondOperand;
+    },
+    division: function() {
+        return this.firstOperand / this.secondOperand;
+    },
+}
