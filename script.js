4e4449556990671c2a9da6bd4b422f4a808ce7d8
cnuneze <23690843+cnuneze@users.noreply.github.com>
2025-01-24 18:42:52 -0300

    Feat: Add button function to print numbers in display

--- script.js
+++ script.js
@@ -1,37 +1,55 @@
 console.log("there")
+// Calculator Logic
 const calculator = {
-    firstOperand: 0,
-    secondOperand: 0,
-    operation: '',
-    operate: function() {
-        switch (this.operation) {
-            case '+':
-                return this.addition();
+  firstOperand: 0,
+  secondOperand: 0,
+  operation: '',
+  operate: function() {
+    switch (this.operation) {
+      case '+':
+        return this.addition();
 
-            case '-':
-                return this.subtraction();
+      case '-':
+        return this.subtraction();
 
-            case '*':
-                return this.multiplication();
+      case '*':
+        return this.multiplication();
 
-            case '/':
-                return this.division();
+      case '/':
+        return this.division();
 
-            default:
-                return 0;
+      default:
+        return 0;
 
-        }
-    },
-    addition: function() {
-        return this.firstOperand + this.secondOperand;
-    },
-    subtraction: function() {
-        return this.firstOperand - this.secondOperand;
-    },
-    multiplication: function() {
-        return this.firstOperand * this.secondOperand;
-    },
-    division: function() {
-        return this.firstOperand / this.secondOperand;
-    },
+    }
+  },
+  addition: function() {
+    return this.firstOperand + this.secondOperand;
+  },
+  subtraction: function() {
+    return this.firstOperand - this.secondOperand;
+  },
+  multiplication: function() {
+    return this.firstOperand * this.secondOperand;
+  },
+  division: function() {
+    return this.firstOperand / this.secondOperand;
+  },
 }
+
+// Events
+const numberButtons = document.querySelectorAll('.btn-number');
+const operationButtons = document.querySelectorAll('.btn-operation')
+
+numberButtons.forEach(button => {
+  button.addEventListener('click', event => {
+    const display = document.querySelector('.operation-text');
+    const valueButton = button.value;
+    if (display.classList.contains('clear')) {
+      display.textContent = '';
+      display.classList.remove('clear');
+    }
+
+    display.textContent += valueButton;
+  })
+});
